f4626bce9e8793a7e6acd5cf61e04f7aa994a35f
Luka Kruljac <lkruljac@domain.local>
2020-07-30 17:46:02 +0200

    first commit

--- adapter.cs
+++ adapter.cs
@@ -1,8 +1,102 @@
-﻿'Infineon.exe' (CoreCLR: DefaultDomain): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.6\System.Private.CoreLib.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'Infineon.exe' (CoreCLR: clrhost): Loaded 'C:\Users\lkruljac\source\repos\Infineon\Infineon\bin\Debug\netcoreapp3.1\Infineon.dll'. Symbols loaded.
-'Infineon.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.6\System.Runtime.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'Infineon.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.6\System.Console.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'Infineon.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.6\System.Threading.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'Infineon.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.6\System.Runtime.Extensions.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'Infineon.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.1.6\System.Text.Encoding.Extensions.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-The program '[15200] Infineon.exe' has exited with code 0 (0x0).
+﻿using System;
+
+namespace Infineon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //existing objects
+            Rectangle rectangle = new Rectangle(1, 2, 3, 4, 5, 6, 7, 8);
+            Triangle triangle = new Triangle(1, 2, 3, 4, 5, 6);
+            //using in way described in interface
+            new RectangleAdapter(rectangle).PrintPoints();
+            new TriangleAdapter(triangle).PrintPoints();
+        }
+    }
+    class Point
+    {
+        public int x;
+        public int y;
+        public String ToString()
+        {
+            return "(" + this.x + "," + this.y + ")";
+        }
+    }
+    interface GeomtricFigure
+    {
+        void PrintPoints()
+        {
+        }
+    }
+    class RectangleAdapter : GeomtricFigure
+    {
+        Rectangle rectangle;
+        public RectangleAdapter(Rectangle rectangle)
+        {
+            this.rectangle = rectangle;
+        }
+        public void PrintPoints()
+        {
+            this.rectangle.PrintRectangle();
+        }
+    }
+    class TriangleAdapter : GeomtricFigure
+    {
+        Triangle triangle;
+        public TriangleAdapter(Triangle triangle)
+        {
+            this.triangle = triangle;
+        }
+        public void PrintPoints()
+        {
+            this.triangle.PrintTriangle();
+        }
+    }
+    class Rectangle
+    {
+        Point A = new Point();
+        Point B = new Point();
+        Point C = new Point();
+        Point D = new Point();
+        public Rectangle(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+        {
+            this.A.x = x1;
+            this.A.y = y1;
+            this.B.x = x2;
+            this.B.y = y2;
+            this.C.x = x3;
+            this.C.y = y3;
+            this.D.x = x4;
+            this.D.y = y4;
+        }
+        public void PrintRectangle()
+        {
+            Console.Write("\nA:" + this.A.ToString());
+            Console.Write("B:" + this.B.ToString());
+            Console.Write("C:" + this.C.ToString());
+            Console.Write("D:" + this.D.ToString());
+        }
+    }
+    class Triangle
+    {
+        Point A = new Point();
+        Point B = new Point();
+        Point C = new Point();
+        public Triangle(int x1, int y1, int x2, int y2, int x3, int y3)
+        {
+            this.A.x = x1;
+            this.A.y = y1;
+            this.B.x = x2;
+            this.B.y = y2;
+            this.C.x = x3;
+            this.C.y = y3;
+        }
+        public void PrintTriangle()
+        {
+            Console.Write("\nA:" + this.A.ToString());
+            Console.Write("B:" + this.B.ToString());
+            Console.Write("C:" + this.C.ToString());
+        }
+    }
+}
